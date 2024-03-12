ebd6d0760fa4373ebf018cf189bbfc4ee70a1466
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-03-12 17:03:36 +0900

    :sparkles: 카카오 4번! 풀이 방향은 보긴했음!
    뭔가를 소모해야지 넘어가는 조건이면 일단 다 가지고 가며 저장하고 필요할 때 하나씩 우선순위 조건에 따라 사용하는 방향으로 가야겠다!

--- Baekjun/2024kakaointern/4.py
+++ Baekjun/2024kakaointern/4.py
@@ -1,3 +1,50 @@
+# 실제 풀이
+def solution(coin, cards):
+    answer = 0
+    dic = {}
+    l = len(cards)
+    can_count = 1
+    for i in range(l // 3):
+        if (l + 1 - cards[i]) in dic:
+            can_count += 1
+        dic[cards[i]] = i
+        
+    ndic={}
+    oc = 0
+    tc = 0
+    i = l // 3
+    while i < l and can_count > 0:
+        can_count -= 1
+        answer += 1 
+        if (l + 1 - cards[i]) in dic:
+            oc += 1
+        if (l + 1 - cards[i+1]) in dic:
+            oc += 1
+        if (l + 1 - cards[i]) in ndic:
+            tc += 1
+        ndic[cards[i]] = i
+        if (l + 1 - cards[i+1]) in ndic:
+            tc += 1
+        else:
+            ndic[cards[i+1]] = i+1
+        if can_count == 0:
+            if coin > 0 and oc > 0:
+                can_count += 1
+                oc -= 1
+                coin -= 1
+            elif coin > 1 and tc > 0:
+                can_count += 1
+                tc -= 1
+                coin -= 2
+                
+        i += 2
+        
+    if i == l and can_count > 0:
+        answer += 1
+            
+    return answer
+
+# 카카오 코테칠 때 풀었던 풀이.
 # 현재 가진 패에서 n+1만드려면 필요한 카드 맥시멈 값.
 # 코인 + 내면서 갈 수 있는 최댓값.
 # 카드 내는게 우선 but 다음 숫자에 필요한 숫자가 나오면 코인 사용
