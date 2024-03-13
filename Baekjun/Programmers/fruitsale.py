b8e22f9d6c70376d31c179c4f744bb48c76198ba
rjsdn0124 <201802145@o.cnu.ac.kr>
2024-03-13 15:17:11 +0900

    :sparkles: 과일장수

---
+++ Baekjun/Programmers/fruitsale.py
@@ -0,0 +1,8 @@
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    for i in range(0,len(score), m):
+        if i + m - 1 >= len(score):
+            break
+        answer += score[i+m-1] * m
+    return answer
