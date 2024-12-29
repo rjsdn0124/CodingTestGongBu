fc696894fc5b216424658c2ff2527836654519ac
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-12-29 16:39:22 +0900

    [Gold IV] Title: 인구 이동, Time: 484 ms, Memory: 295348 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -10,12 +10,12 @@ public class Main{
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[][] arr = getInput(br);
+		Section[][] arr = getInput(br);
 		int result = solution(arr);
 		System.out.println(result);
 	}
 
-	public static int solution(int[][] arr){
+	public static int solution(Section[][] arr){
 		int result = 0;
 		while(true){
 			boolean isUpdated = false;
@@ -38,9 +38,9 @@ public class Main{
 		return result;
 	}
 
-	public static boolean bfs(int x, int y, int[][] arr, boolean[][] visited){
+	public static boolean bfs(int x, int y, Section[][] arr, boolean[][] visited){
 		Queue<int[]> q = new LinkedList<>();
-		List<int[]> xys = new LinkedList<>();
+		Section s = new Section(arr[y][x].getValue());
 		q.add(new int[] { x, y });
 		visited[y][x] = true;
 		int sum = 0;
@@ -48,15 +48,16 @@ public class Main{
 
 		while(!q.isEmpty()){
 			int[] xy = q.poll();
-			xys.add(xy);
-			sum += arr[xy[1]][xy[0]];
+			int prev = arr[xy[1]][xy[0]].getValue();
+			sum += prev;
+			arr[xy[1]][xy[0]] = s;
 			count++;
 
 			for(int i = 0; i < 4; i++){
 				int nx = xy[0] + dx[i];
 				int ny = xy[1] + dy[i];
 				if(0 <= nx && nx < n && 0 <= ny && ny < n && !visited[ny][nx]){
-					int gap = Math.abs(arr[xy[1]][xy[0]] - arr[ny][nx]);
+					int gap = Math.abs(prev - arr[ny][nx].getValue());
 					if(min <= gap && gap <= max){
 						q.add(new int[]{nx, ny});
 						visited[ny][nx] = true;
@@ -64,31 +65,45 @@ public class Main{
 				}
 			}
 		}
+
 		if(count > 1){
 			int avg = sum / count;
-			for(int[] xy: xys){
-				arr[xy[1]][xy[0]] = avg;
-			}
+			s.setValue(avg);
 			return true;
 		}
 		return false;
 	}
 
-	private static int[][] getInput(BufferedReader br) throws IOException{
+	private static Section[][] getInput(BufferedReader br) throws IOException{
 		// 입력.
 		String[] input = br.readLine().split(" ");
 		n = Integer.parseInt(input[0]);
 		min = Integer.parseInt(input[1]);
 		max = Integer.parseInt(input[2]);
 
-		int[][] arr = new int[n][n];
+		Section[][] arr = new Section[n][n];
 
 		for(int i = 0; i < n; i++){
 			input = br.readLine().split(" ");
 			for(int j = 0; j < n; j++){
-				arr[i][j] = Integer.parseInt(input[j]);
+				arr[i][j] = new Section(Integer.parseInt(input[j]));
 			}
 		}
 		return arr;
+	}
+}
+
+class Section{
+	int value;
+	public Section(int value){
+		this.value = value;
+	}
+
+	public int getValue(){
+		return this.value;
+	}
+
+	public void setValue(int value){
+		this.value = value;
 	}
 }
