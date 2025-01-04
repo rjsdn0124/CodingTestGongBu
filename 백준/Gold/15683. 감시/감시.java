cc0d2b2135d4ffce7f41ab1014e67d3cdc4e02c5
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-04 17:07:36 +0900

    [Gold III] Title: 감시, Time: 140 ms, Memory: 16880 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -7,9 +7,9 @@ public class Main{
 	private static int result;
 	private static int zeroCount = 0;
 	private static int[][] arr;
-	private static int[] dx = new int[]{0 , 1, 0, -1};
-	private static int[] dy = new int[]{-1 , 0, 1, 0};
-	private static int[][] dInds = new int[][]{
+	private static int[] dx = {0 , 1, 0, -1};
+	private static int[] dy = {-1 , 0, 1, 0};
+	private static int[][] dInds = {
 		{},
 		{0},
 		{1, 3},
@@ -17,42 +17,41 @@ public class Main{
 		{0,1,2},
 		{0,1,2,3}
 	};
-	private static int[] rTime = new int[]{0, 4, 2, 4, 4, 1};
+	private static int[] rTime = {0, 4, 2, 4, 4, 1};
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		getInput(br);
-		backTracking(0, 0, 7, 0);
+		List<Integer> cctvs = getInput(br);
+		backTracking(cctvs, 0, 0);
 		System.out.println(zeroCount - result);
 	}
 
-	private static void backTracking(int x, int y, int cctvInd, int count){
-		for(int i = y; i < N; i++){
-			for(int j = x; j < M; j++){
-				if(arr[i][j] > 0 && arr[i][j] < 6){
-					// 돌리기
-					for(int k = 0; k < rTime[arr[i][j]]; k++){
-						// 커버 범위 찾기
-						int tc = updateArr(j, i, cctvInd, k);
-						count += tc;
-						// 다음 cctv 이동
-						backTracking(j + 1, i, cctvInd + 1, count);
-						// 원상 복구
-						updateArr(j, i, cctvInd, k);
-						count -= tc;
-					}
-
-					return;
-				}
-			}
-			x = 0;
+	private static void backTracking(List<Integer> cctvs, int i, int count){
+		if(cctvs.size() <= i) {
+			result = Math.max(result, count);
+			return;
 		}
-		result = Math.max(result, count);
+		int xy = cctvs.get(i);
+		int x = xy / 10;
+		int y = xy % 10;
+
+		// 돌리기
+		for(int k = 0; k < rTime[arr[y][x]]; k++){
+			// 커버 범위 찾기
+			int tc = updateArr(x, y, i, k);
+			count += tc;
+			// 다음 cctv 이동
+			backTracking(cctvs, i + 1, count);
+			// 원상 복구
+			updateArr(x, y, i, k);
+			count -= tc;
+		}
 	}
 
 	private static int updateArr(int x, int y, int cctvInd, int rotateW){
 		int[] dInd = dInds[arr[y][x]];
 		int count = 0;
+		cctvInd += 7;
 
 		for(int i: dInd){
 			i = (i + rotateW) % 4;
@@ -76,22 +75,26 @@ public class Main{
 		return count;
 	}
 
-	private static void getInput(BufferedReader br) throws IOException {
+	private static List<Integer> getInput(BufferedReader br) throws IOException {
 		// 입력.
 		String[] input = br.readLine().split(" ");
 		N = Integer.parseInt(input[0]);
 		M = Integer.parseInt(input[1]);
 
 		arr = new int[N][M];
-
+		List<Integer> cctvs = new ArrayList<>();
 		for(int i = 0; i < N; i++){
 			input = br.readLine().split(" ");
 			for(int j = 0; j < M; j++){
 				arr[i][j] = Integer.parseInt(input[j]);
 				if(arr[i][j] == 0){
 					zeroCount++;
+				}else if(arr[i][j] < 6){
+					cctvs.add(j * 10 + i);
 				}
 			}
 		}
+
+		return cctvs;
 	}
 }
