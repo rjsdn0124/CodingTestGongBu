53c638239b59be22717f3b0d6622b1072fe164d2
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-13 01:06:07 +0900

    [Gold II] Title: 성냥개비, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Gold/3687. 성냥개비/성냥개비.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	private static int N;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = getInput(br);
+		StringBuilder sb = new StringBuilder();
+		solution(arr, sb);
+		System.out.println(sb);
+	}
+
+	private static void solution(int[] arr, StringBuilder sb){
+		long[] dp = dp();
+		long min;
+		for(int i: arr){
+			min = findMin(i, dp);
+			sb.append(min);
+			sb.append(" ");
+			findMax(i, sb);
+			sb.append("\n");
+		}
+	}
+
+	private static long[] dp() {
+		long[] dp = new long[101];
+		dp[1] = 9;
+		dp[2] = 1;
+		dp[3] = 7;
+		dp[4] = 4;
+		dp[5] = 2;
+		dp[6] = 6;
+		dp[7] = 8;
+		int[] map = {1, 7, 4, 2, 0, 8};
+
+		for(int i = 8; i < dp.length; i++){
+			dp[i] = Long.MAX_VALUE;
+			for(int j = 0; j < 6; j++){
+				dp[i] = Math.min(dp[i], dp[i - (j + 2)] * 10 + map[j]);
+			}
+		}
+
+		return dp;
+	}
+
+	private static long findMin(int torchCount, long[] dp){
+		return dp[torchCount];
+	}
+
+
+	private static void findMax(int torchCount, StringBuilder sb){
+		if(torchCount % 2 == 1){
+			sb.append(7);
+			torchCount -= 3;
+		}
+		while(torchCount > 0){
+			sb.append(1);
+			torchCount -= 2;
+		}
+	}
+
+	private static int[] getInput(BufferedReader br) throws IOException{
+		N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		for(int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		return arr;
+	}
+}
