e96da254ca35cab72a226a3b7e05bb5d5bdd3b93
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-13 22:19:48 +0900

    [Gold II] Title: 미확인 도착지, Time: 500 ms, Memory: 67908 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main{
 	private static int v,e,t,s,g,h;
-	private static List<int[]>[] edges;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,6 +13,9 @@ public class Main{
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
 		String[] line;
+		Queue<Integer> result = new PriorityQueue<>();
+		List<Integer>[] edges = new ArrayList[2001];
+		Queue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
 
 		while(n-- > 0){
 			line = br.readLine().split(" ");
@@ -25,7 +27,6 @@ public class Main{
 			g = Integer.parseInt(line[1]);
 			h = Integer.parseInt(line[2]);
 
-			edges = new ArrayList[v + 1];
 			for(int i = 1; i <= v; i++){
 				edges[i] = new ArrayList<>();
 			}
@@ -36,19 +37,20 @@ public class Main{
 				n1 = Integer.parseInt(line[0]);
 				n2 = Integer.parseInt(line[1]);
 				w = Integer.parseInt(line[2]);
-				edges[n1].add(new int[]{n2, w});
-				edges[n2].add(new int[]{n1, w});
+				edges[n1].add(createMyEW(n2, w));
+				edges[n2].add(createMyEW(n1, w));
 			}
 
-			Set<Integer> targets = new HashSet<>();
+			boolean[] isPassed = new boolean[v + 1];
+			dijk(q, isPassed, edges);
 			for(int i = 0; i < t; i++){
-				targets.add(Integer.parseInt(br.readLine()));
+				int res = Integer.parseInt(br.readLine());
+				if(isPassed[res]) {
+					result.add(res);
+				}
 			}
-
-			List<Integer> result = dijk(targets);
-			Collections.sort(result);
-			for(int i = 0; i < result.size(); i++){
-				sb.append(result.get(i)).append(" ");
+			while(!result.isEmpty()){
+				sb.append(result.poll()).append(" ");
 			}
 			sb.append("\n");
 		}
@@ -56,49 +58,39 @@ public class Main{
 		return sb;
 	}
 
-	private static List<Integer> dijk(Set<Integer> targets){
-		Queue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-		List<Integer> result = new ArrayList<>();
+	private static void dijk(Queue<int[]> q, boolean[] isPassed, List<Integer>[] edges){
+		q.add(new int[]{s, 0});
 		int[] shortPath = new int[v + 1];
-		boolean[] isPassed = new boolean[v + 1];
-		int[] edge;
-		int nowWeight;
-		q.add(new int[]{s, 0, 0});
 		Arrays.fill(shortPath, Integer.MAX_VALUE);
 
 		shortPath[s] = 0;
 		while(!q.isEmpty()){
-			edge = q.poll();
+			int[] edge = q.poll();
 			if(shortPath[edge[0]] < edge[1]) {
 				continue;
 			}
 
-			if(targets.contains(edge[0]) && isPassed[edge[0]]){
-				result.add(edge[0]);
-				targets.remove(edge[0]);
-				if(targets.isEmpty()){
-					return result;
-				}
-			}
-
-			for(int[] te: edges[edge[0]]){
-				nowWeight = te[1] + edge[1];
-				if(shortPath[te[0]] > nowWeight) {
+			for(int tew: edges[edge[0]]){
+				int te = tew / 1001;
+				int nowWeight = tew % 1001 + edge[1];
+				if(shortPath[te] > nowWeight) {
 					// 다음 목적지 갱신 가능하면 q에 추가.
-					shortPath[te[0]] = nowWeight;
-					q.add(new int[]{te[0], nowWeight});
-					isPassed[te[0]] = isPassed[edge[0]] || checkIsPassed(edge, te[0]);
-				}else if(shortPath[te[0]] == nowWeight && !isPassed[te[0]]){
-					// 다음 목적지 결과가 같으면 gh 지난 친구만 넣기.
-					if(isPassed[edge[0]]) isPassed[te[0]] = true;
-					else isPassed[te[0]] = checkIsPassed(edge, te[0]);
+					shortPath[te] = nowWeight;
+					q.add(new int[]{te, nowWeight});
+					isPassed[te] = isPassed[edge[0]] || (checkIsPassed(edge[0]) && checkIsPassed(te));
+				}else if(shortPath[te] == nowWeight && !isPassed[te]){
+					// 다음 목적지 결과가 같으면 gh 지났는지 체크
+					isPassed[te] = isPassed[edge[0]] || (checkIsPassed(edge[0]) && checkIsPassed(te));
 				}
 			}
 		}
-		return result;
 	}
 
-	private static boolean checkIsPassed(int[] edge, int tn){
-		return (edge[0] == g || edge[0] == h) && (tn == g || tn == h);
+	private static int createMyEW(int e, int w){
+		return e * 1001 + w;
+	}
+
+	private static boolean checkIsPassed(int node){
+		return (node == g || node == h);
 	}
 }
