c6a9d27b7aa7edd30592c4165251736da176e150
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-14 17:42:45 +0900

    [Gold I] Title: 횡단보도, Time: 1348 ms, Memory: 282168 KB -BaekjoonHub

---
+++ 백준/Gold/24042. 횡단보도/횡단보도.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	private static int N;
+	private static long M;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<int[]>[] map = getInput(br);
+		System.out.println(solution(map));
+	}
+
+	private static long solution(List<int[]>[] map){
+		Queue<int[]> pq = new PriorityQueue<>(Comparator.comparingLong(a->(long)a[2] * M + a[1]));
+		pq.add(new int[]{ 1, 0, 0 });
+		long[] updatedArr = new long[N + 1];
+		Arrays.fill(updatedArr, Long.MAX_VALUE);
+		updatedArr[1] = 0;
+
+		while(!pq.isEmpty()){
+			int[] info = pq.poll();
+			long spentTime = calSpentTime(info[1], info[2]);
+
+			if(updatedArr[info[0]] >= spentTime){
+				if(info[0] == N) {
+					return spentTime;
+				}
+
+				for(int[] cw : map[info[0]]){
+					long tst = calSpentTime(cw[1] + 1, info[2]);
+					if(updatedArr[cw[0]] > tst && cw[1] >= info[1]){
+						pq.add(new int[]{cw[0], cw[1] + 1, info[2]});
+						updatedArr[cw[0]] = tst;
+					}else if(updatedArr[cw[0]] > tst + M){
+						pq.add(new int[]{cw[0], cw[1] + 1, info[2] + 1});
+						updatedArr[cw[0]] = tst + M;
+					}
+				}
+			}
+		}
+
+		return -1;
+	}
+
+	private static long calSpentTime(int time, int cycle){
+		return cycle * M + time;
+	}
+
+	private static List<int[]>[] getInput(BufferedReader br) throws IOException{
+		String[] line = br.readLine().split(" ");
+		N = Integer.parseInt(line[0]);
+		M = Integer.parseInt(line[1]);
+
+		List<int[]>[] map = new LinkedList[N + 1];
+
+		for(int i = 1; i <= N; i++){
+			map[i] = new LinkedList<>();
+		}
+
+		for(int i = 0 ; i < M; i++){
+			line = br.readLine().split(" ");
+			int n1 = Integer.parseInt(line[0]);
+			int n2 = Integer.parseInt(line[1]);
+			map[n1].add(new int[]{n2, i});
+			map[n2].add(new int[]{n1, i});
+		}
+
+		return map;
+	}
+}
