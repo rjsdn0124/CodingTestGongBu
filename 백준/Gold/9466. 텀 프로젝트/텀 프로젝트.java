d4f63cbf506a79d3aa8e28de0463d0c2287c0ac6
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-15 17:46:44 +0900

    [Gold III] Title: 텀 프로젝트, Time: 908 ms, Memory: 302984 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,7 +1,8 @@
 import java.io.*;
-import java.util.*;
 
 public class Main{
+	private static int[] students;
+	private static int[] visited;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,41 +18,36 @@ public class Main{
 			int studentCount = Integer.parseInt(br.readLine());
 
 			String[] line = br.readLine().split(" ");
-			int[] students = new int[studentCount];
-			boolean[] visited = new boolean[studentCount];
-			int[] visi = new int[studentCount];
-			int totalTeamMateCount = 0;
+			students = new int[studentCount];
+			visited = new int[studentCount];
 
 			for (int i = 0; i < studentCount; i++) {
 				students[i] = Integer.parseInt(line[i]) - 1;
-				if(i == students[i]){
-					totalTeamMateCount++;
-					visi[i] = -1;
+			}
+
+			for(int i = 0; i < students.length; i++){
+				if(visited[i] == 0) {
+					studentCount -= createTeam(i, 0);
 				}
 			}
 
-			for(int i = 0; i < studentCount; i++){
-				if(visi[i] == 0) {
-					totalTeamMateCount += createTeam(visi, students, i, 1);
-				}
-			}
-
-			sb.append(studentCount - totalTeamMateCount).append("\n");
+			sb.append(studentCount).append("\n");
 		}
 
 		return sb;
 	}
 
-	private static int createTeam(int[] visited, int[] students, int ind, int result){
-		if(visited[ind] > 0){
-			return result - visited[ind];
-		}else if(visited[ind] == 0){
-			visited[ind] = result++;
-			result = createTeam(visited, students, students[ind], result);
-			visited[ind] = -1;
-			return result;
+	private static int createTeam(int ind, int result){
+		visited[ind] = ++result;
+		if(visited[students[ind]] == 0) {
+			result = createTeam(students[ind], result);
+		}else if(visited[students[ind]] > 0){
+			result -= visited[students[ind]] - 1;
 		}else{
-			return 0;
+			result = 0;
 		}
+		visited[ind] = -1;
+
+		return result;
 	}
 }
