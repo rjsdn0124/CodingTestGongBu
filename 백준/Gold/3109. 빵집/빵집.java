efbb1f42db0c2d5c9b155b983d8aa53d9eac0e37
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-16 14:12:56 +0900

    [Gold II] Title: 빵집, Time: 336 ms, Memory: 29580 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main{
 	private static int N, M;
 	private static boolean[][] arr;
-	private static int[] dy = {-1, 0, 1};
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +14,7 @@ public class Main{
 
 	private static int solution(){
 		int result = 0;
-		for(int i = 0; i < N; i++){
+		for(int i = 1; i <= N; i++){
 			if(arr[i][0] && dfs(0, i)){
 				result++;
 			}
@@ -24,13 +23,16 @@ public class Main{
 	}
 
 	private static boolean dfs(int x, int y){
-		arr[y][x++] = false;
-		if(x == M) return true;
+		if(++x == M) return true;
+		y--;
 		for(int i = 0; i < 3; i++){
-			int ny = y + dy[i];
-			if(0 <= ny && ny < N && arr[ny][x] && dfs(x, y+dy[i])){
-				return true;
+			if(arr[y][x]){
+				arr[y][x] = false;
+				if(dfs(x, y)) {
+					return true;
+				}
 			}
+			y++;
 		}
 		return false;
 	}
@@ -40,9 +42,9 @@ public class Main{
 		N = Integer.parseInt(line[0]);
 		M = Integer.parseInt(line[1]) - 2;
 
-		arr = new boolean[N][M];
+		arr = new boolean[N + 2][M];
 
-		for(int i = 0; i < N; i++){
+		for(int i = 1; i <= N; i++){
 			String l = br.readLine();
 			for(int j = 0; j < M; j++){
 				arr[i][j] = l.charAt(j + 1) == '.';
