65f682d0ae4f1e712eeebfb1ba900224c977ffa9
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-16 13:51:15 +0900

    [Gold II] Title: 빵집, Time: 336 ms, Memory: 29628 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -16,7 +16,7 @@ public class Main{
 	private static int solution(){
 		int result = 0;
 		for(int i = 0; i < N; i++){
-			if(arr[i][1] && dfs(1, i)){
+			if(arr[i][0] && dfs(0, i)){
 				result++;
 			}
 		}
@@ -24,9 +24,8 @@ public class Main{
 	}
 
 	private static boolean dfs(int x, int y){
-		arr[y][x] = false;
-		x++;
-		if(x == M - 1) return true;
+		arr[y][x++] = false;
+		if(x == M) return true;
 		for(int i = 0; i < 3; i++){
 			int ny = y + dy[i];
 			if(0 <= ny && ny < N && arr[ny][x] && dfs(x, y+dy[i])){
@@ -39,14 +38,14 @@ public class Main{
 	private static void getInput(BufferedReader br) throws IOException{
 		String[] line = br.readLine().split(" ");
 		N = Integer.parseInt(line[0]);
-		M = Integer.parseInt(line[1]);
+		M = Integer.parseInt(line[1]) - 2;
 
 		arr = new boolean[N][M];
 
 		for(int i = 0; i < N; i++){
 			String l = br.readLine();
 			for(int j = 0; j < M; j++){
-				arr[i][j] = l.charAt(j) == '.';
+				arr[i][j] = l.charAt(j + 1) == '.';
 			}
 		}
 	}
