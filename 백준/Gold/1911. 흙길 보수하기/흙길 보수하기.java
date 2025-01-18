9eae47aa6fe94555cda18baa7e4d44c279a52d63
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-18 13:34:51 +0900

    [Gold V] Title: 흙길 보수하기, Time: 236 ms, Memory: 21464 KB -BaekjoonHub

---
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	private static int N, M;
+	private static int[][] arr;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		getInput(br);
+		int result = solution();
+		System.out.println(result);
+	}
+
+	private static int solution(){
+		int result = 0;
+		int start = 0;
+
+		for(int i = 0; i < N; i++){
+			start = Math.max(arr[i][0], start);
+			if(start < arr[i][1]){
+				int sToE = (arr[i][1] - start - 1) / M + 1;
+				result += sToE;
+				start = start + sToE * M;
+			}
+		}
+
+		return result;
+	}
+
+	private static void getInput(BufferedReader br) throws IOException{
+		String[] line = br.readLine().split(" ");
+		N = Integer.parseInt(line[0]);
+		M = Integer.parseInt(line[1]);
+		arr = new int[N][2];
+
+		for(int i = 0; i < N; i++){
+			line = br.readLine().split(" ");
+			arr[i] = new int[]{Integer.parseInt(line[0]), Integer.parseInt(line[1])};
+		}
+
+		Arrays.sort(arr, new Comparator<int[]>(){
+			@Override
+			public int compare(int[] a, int[] b){
+				return a[1] - b[1];
+			}
+		});
+	}
+}
