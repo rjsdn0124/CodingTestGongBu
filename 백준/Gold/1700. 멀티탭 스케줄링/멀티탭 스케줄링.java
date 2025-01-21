1ed7b8541f71158a50d78b0e05d4c94b6158aa4f
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-21 14:23:57 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main{
 	private static int N, M, now = 0, result = 0;
 	private static int[] arr;
-	private static Electronic[] electronics;
+	private static List<Integer>[] electronics;
 	private static Set<Integer> set = new HashSet<>();
 
 	public static void main(String[] args) throws IOException{
@@ -19,27 +19,25 @@ public class Main{
 		// 일단 멀티탭 가득 채울 때까지 계속 set에 저장.
 		while(set.size() < N && now < M){
 			set.add(arr[now]);
-			electronics[arr[now++]].removeElement();
+			electronics[arr[now++]].remove(0);
 		}
 
 		// 가득 찼을 때.
 		while(now < M) {
+			int targetInd = arr[now];
 			// 이미 set에 있는 경우.
-			if(!set.contains(arr[now])){
-				int max = 0;
-				int targetInd = 0;
+			if(!set.contains(targetInd)){
 				// 하나를 무조건 뽑아야함.
 				for(int i : set){
 					// 뽑기 우선순위
 					// 1. 다음에 예정이 없는 친구
-					if(electronics[i].list.size() == 0){
+					if(electronics[i].isEmpty()){
 						targetInd = i;
 						break;
 					}else{
 						// 2. 다음이 가장 먼 친구 - 다음에 예정이 있다면 다시 꼽아야함.
-						if(max < electronics[i].list.get(0)){
+						if(electronics[targetInd].get(0) < electronics[i].get(0)){
 							targetInd = i;
-							max = electronics[i].list.get(0);
 						}
 					}
 
@@ -48,7 +46,7 @@ public class Main{
 				result++;
 			}
 			set.add(arr[now]);
-			electronics[arr[now++]].removeElement();
+			electronics[arr[now++]].remove(0);
 		}
 	}
 
@@ -58,28 +56,16 @@ public class Main{
 		M = Integer.parseInt(line[1]);
 
 		arr = new int[M];
-		electronics = new Electronic[M];
+		electronics = new LinkedList[M];
 
 		for(int i = 0; i < M; i++) {
-			electronics[i] = new Electronic();
+			electronics[i] = new LinkedList<>();
 		}
 
 		String[] l = br.readLine().split(" ");
 		for(int i = 0; i < M; i++) {
 			arr[i] = Integer.parseInt(l[i]) - 1;
-			electronics[arr[i]].list.add(i);
+			electronics[arr[i]].add(i);
 		}
-	}
-}
-
-class Electronic{
-	List<Integer> list;
-
-	public Electronic(){
-		list = new LinkedList<>();
-	}
-
-	public void removeElement(){
-		this.list.remove(0);
 	}
 }
