41346d755e7fa3e8580979bc16bc891a9b83b8d0
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-23 18:37:53 +0900

    [Gold V] Title: 다각형의 면적, Time: 180 ms, Memory: 21008 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.java
@@ -2,32 +2,30 @@ import java.io.*;
 
 public class Main{
 	private static int N;
-	private static double result = 0D;
-	private static int[][] arr;
+	private static long result = 0;
+	private static long[][] arr;
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		init(br);
-		System.out.printf("%.1f", solution());
+		solution();
+		System.out.printf("%.1f", Math.abs(0.5 * result));
 	}
 
-	private static double solution(){
-		double result = 0;
+	private static void solution(){
 		for(int i = 0; i < N; i++){
-			result += 0.5 * arr[i + 1][0] * arr[i][1];
-			result -= 0.5 * arr[i][0] * arr[i + 1][1];
+			result += arr[i + 1][0] * arr[i][1] - arr[i][0] * arr[i + 1][1];
 		}
-		return Math.abs(result);
 	}
 
 	private static void init(BufferedReader br) throws IOException {
 		N = Integer.parseInt(br.readLine());
-		arr = new int[N + 1][2];
+		arr = new long[N + 1][2];
 
 		for(int i = 0; i < N; i++){
 			String[] l = br.readLine().split(" ");
-			arr[i] = new int[]{Integer.parseInt(l[0]), Integer.parseInt(l[1])};
+			arr[i] = new long[]{Long.parseLong(l[0]), Long.parseLong(l[1])};
 		}
 		arr[N] = arr[0];
 	}
