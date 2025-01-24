17ef87aa69c35435feac35fe3b0ff79bfca96653
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-24 17:05:16 +0900

    [Gold IV] Title: 팰린드롬?, Time: 888 ms, Memory: 284316 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	private static int N, M;
+	private static int[] arr;
+	private static int[] s;
+	private static int[] e;
+	private static boolean[][] dp;
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		init(br);
+		solution();
+		System.out.print(sb);
+	}
+
+	private static void solution(){
+		// Dp 배열 만들기. (홀수 짝수 케이스 고려하기)
+		findDp();
+		// dp 에서 찾기.
+		doAnswer();
+	}
+
+	private static void findDp(){
+		for(int i = 0; i < N; i++){
+			int start = i;
+			int end = i;
+			boolean prevResult = true;
+			while(start >= 0 && end < N){
+				if(arr[start] == arr[end]){
+					dp[start][end] = prevResult;
+				}else {
+					prevResult = false;
+				}
+				start--;
+				end++;
+			}
+		}
+
+		for(int i = 0; i < N - 1; i++){
+			int start = i;
+			int end = i + 1;
+			boolean prevResult = true;
+			while(start >= 0 && end < N){
+				if(arr[start] == arr[end]){
+					dp[start][end] = prevResult;
+				}else {
+					prevResult = false;
+				}
+				start--;
+				end++;
+			}
+		}
+	}
+
+	private static void doAnswer(){
+		for(int i = 0; i < M; i++){
+			sb.append(dp[s[i]][e[i]] ? 1 : 0).append("\n");
+		}
+	}
+	private static void init(BufferedReader br) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		dp = new boolean[N][N];
+		String[] l = br.readLine().split(" ");
+		for(int i = 0; i < N; i++){
+			arr[i] = Integer.parseInt(l[i]);
+		}
+		M = Integer.parseInt(br.readLine());
+		s = new int[M];
+		e = new int[M];
+		for(int i = 0; i < M; i++){
+			l = br.readLine().split(" ");
+			s[i] = Integer.parseInt(l[0]) - 1;
+			e[i] = Integer.parseInt(l[1]) - 1;
+		}
+	}
+}
