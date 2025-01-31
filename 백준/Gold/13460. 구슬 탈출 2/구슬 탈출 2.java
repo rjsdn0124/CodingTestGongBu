f5369f766581443dabe4609f7c7e69ca50ffad02
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-01-31 15:28:00 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 152 ms, Memory: 21276 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -0,0 +1,186 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	private static int N, M, result = -1;
+	private static boolean isPassedBlueHole = false, isPassedRedHole = false;
+	private static int[] blue, red, hole;
+	private static boolean[][] arr;
+	private static int[] dx = new int[]{1, -1, 0, 0};
+	private static int[] dy = new int[]{0, 0, 1, -1};
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		init(br);
+		solution();
+		System.out.print(result);
+	}
+
+	private static void solution(){
+		// dfs 돌면서 위아래 좌우 기울이기.
+		bfs();
+	}
+
+	private static void bfs(){
+		Queue<int[]> q = new LinkedList<>();
+		q.add(new int[] {blue[0], blue[1], red[0], red[1], -1, 0});
+		while(!q.isEmpty()){
+			int[] temp = q.poll();
+			if(temp[5] == 10){
+				return;
+			}
+			temp[5]++;
+			for(int i = 0; i < 4; i++){
+				blue[0] = temp[0];
+				blue[1] = temp[1];
+				red[0] = temp[2];
+				red[1] = temp[3];
+				// 기울였는데 둘 다 위치 안 바뀌면 패스.
+				if(i != temp[4] && move(i)){
+					// 빨강이 빠지면 같은 타이밍에 파랑 빠지는지 체크.
+					if(isPassedBlueHole){
+						isPassedBlueHole = false;
+						continue;
+					}else if(isPassedRedHole){
+						result = temp[5];
+						return;
+					}
+					q.add(new int[] {blue[0], blue[1], red[0], red[1], i, temp[5]});
+				}
+			}
+		}
+	}
+
+	private static boolean move(int direction){
+		boolean isMoved = false;
+		// x축 y축 둘 중 하나가 같은 위치일 때 먼저 도착하는 애 있으면 그만하기
+		if(direction < 2 && blue[1] == red[1]){
+			boolean isRedBig = (blue[0] < red[0] && direction % 2 == 0) || (blue[0] > red[0] && direction % 2 == 1);
+			boolean isMetHole = false;
+			blue[0] += dx[direction];
+			isMoved = arr[blue[1]][blue[0]];
+			while(arr[blue[1]][blue[0]]){
+				isMetHole |= blue[0] == hole[0];
+				// blue 위치 업데이트.
+				blue[0] += dx[direction];
+			}
+			blue[0] -= dx[direction];
+			// 만났을 경우
+			if(hole[1] == blue[1] && isMetHole){
+				isPassedBlueHole = true;
+				return true;
+			}
+			isMetHole = false;
+			red[0] += dx[direction];
+			isMoved |= arr[red[1]][red[0]];
+			while(arr[red[1]][red[0]]){
+				isMetHole |= red[0] == hole[0];
+				// red 위치 업데이트.
+				red[0] += dx[direction];
+			}
+			red[0] -= dx[direction];
+			if(hole[1] == red[1] && isMetHole){
+				isPassedRedHole = true;
+				return true;
+			}
+			if(blue[0] == red[0]){
+				if(isRedBig){
+					blue[0] -= dx[direction];
+				}else{
+					red[0] -= dx[direction];
+				}
+			}
+		}else if(direction >= 2 && blue[0] == red[0]){
+			boolean isRedBig = (blue[1] < red[1] && direction % 2 == 0) || (blue[1] > red[1] && direction % 2 == 1);
+			boolean isMetHole = false;
+			blue[1] += dy[direction];
+			isMoved = arr[blue[1]][blue[0]];
+			while(arr[blue[1]][blue[0]]){
+				isMetHole |= blue[1] == hole[1];
+				// blue 위치 업데이트.
+				blue[1] += dy[direction];
+			}
+			blue[1] -= dy[direction];
+			// 만났을 경우
+			if(hole[0] == blue[0] && isMetHole){
+				isPassedBlueHole = true;
+				return true;
+			}
+			isMetHole = false;
+			red[1] += dy[direction];
+			isMoved |= arr[red[1]][red[0]];
+			while(arr[red[1]][red[0]]){
+				isMetHole |= red[1] == hole[1];
+				// red 위치 업데이트.
+				red[1] += dy[direction];
+			}
+			red[1] -= dy[direction];
+			if(hole[0] == red[0] && isMetHole){
+				isPassedRedHole = true;
+				return true;
+			}
+			if(blue[1] == red[1]){
+				if(isRedBig){
+					blue[1] -= dy[direction];
+				}else{
+					red[1] -= dy[direction];
+				}
+			}
+		}else{
+			blue[0] += dx[direction];
+			blue[1] += dy[direction];
+			isMoved = arr[blue[1]][blue[0]];
+			while(arr[blue[1]][blue[0]]){
+				if(blue[0] == hole[0] && blue[1] == hole[1]){
+					isPassedBlueHole = true;
+					return true;
+				}
+				// blue 위치 업데이트.
+				blue[0] += dx[direction];
+				blue[1] += dy[direction];
+			}
+			blue[0] -= dx[direction];
+			blue[1] -= dy[direction];
+			// red 움지기기
+			red[0] += dx[direction];
+			red[1] += dy[direction];
+			isMoved |= arr[red[1]][red[0]];
+			while(arr[red[1]][red[0]]){
+				if(red[0] == hole[0] && red[1] == hole[1]){
+					isPassedRedHole = true;
+					return true;
+				}
+				// red 위치 업데이트.
+				red[0] += dx[direction];
+				red[1] += dy[direction];
+			}
+			red[0] -= dx[direction];
+			red[1] -= dy[direction];
+		}
+
+		return isMoved;
+	}
+
+	private static void init(BufferedReader br) throws IOException {
+		String[] line = br.readLine().split(" ");
+		N = Integer.parseInt(line[0]);
+		M = Integer.parseInt(line[1]);
+
+		arr = new boolean[N][M];
+
+		for(int i = 0; i < N; i++) {
+			String l = br.readLine();
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = l.charAt(j) != '#';
+				if(l.charAt(j) == 'R'){
+					red = new int[]{j,i};
+				}else if(l.charAt(j) == 'B'){
+					blue = new int[]{j,i};
+				}else if(l.charAt(j) == 'O'){
+					hole = new int[]{j,i};
+				}
+			}
+		}
+	}
+}
