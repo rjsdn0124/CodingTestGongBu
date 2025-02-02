5da716b5a91af8a7e1534e2ae4e1b5b790bc9a00
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-02-02 14:28:58 +0900

    [Platinum V] Title: 등산 마니아, Time: 2272 ms, Memory: 161496 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/20188. 등산 마니아/등산 마니아.java
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.java
@@ -5,6 +5,7 @@ public class Main{
 	private static int N;
 	private static long result = 0;
 	private static List<Integer>[] arr;
+	private static int[] subTree;
 	private static boolean[] visited;
 
 	public static void main(String[] args) throws IOException{
@@ -12,37 +13,25 @@ public class Main{
 
 		getInput(br);
 		solution();
-		System.out.print(result);
+		System.out.print(result / 2);
 	}
 
 	private static void solution(){
-		// dfs 돌기. 인자 -> 자신 ind, depth
-		// dfs에선 무조건 자신을 포함한 자식들만으로 이루어진 조합 계산.
-		// dfs에서 계산해서 갱신해야하는거. 왼쪽 자신 + 오른쪽 자신 + 왼쪽 오른쪽. 모두 depth더해줘야함.
-		// 자신이 정상일 때를 구했으면 실제 정상과 거리 계산해서 결과 갱신.
-		// dfs 반환값은 자신 포함 갯수, 자신 포함 깊이의 합
-		dfs(0, 0);
+		dfs(0);
+
+		for (int i = 1; i < N; i++) {
+			result += (2L * N - subTree[i] - 1) * subTree[i];
+		}
 	}
 
-	private static long[] dfs(int ind, int depth){
-		long count = 0;
-		long dSum = 0;
-		long[] child = new long[2];
+	private static void dfs(int ind){
 		visited[ind] = true;
-		for(int i : arr[ind]){
-			if(!visited[i]){
-				child = dfs(i, depth+1);
-				count += child[0];
-				// 자식 깊이 합 + 자식 개수
-				dSum += child[1] + child[0];
+		for(int n : arr[ind]) {
+			if(!visited[n]) {
+				dfs(n);
+				subTree[ind] += subTree[n];
 			}
 		}
-		long prevC = count - child[0];
-		long prevS = dSum - child[1] - child[0];
-		result += dSum + count * depth;
-		result += prevS * child[0] + (child[0] + child[1]) * prevC + depth * prevC * child[0];
-		count += 1;
-		return new long[]{count, dSum};
 	}
 
 	private static void getInput(BufferedReader br) throws IOException {
@@ -50,6 +39,8 @@ public class Main{
 
 		arr = new ArrayList[N];
 		visited = new boolean[N];
+		subTree = new int[N];
+		Arrays.fill(subTree, 1);
 		for(int i = 0; i < N; i++){
 			arr[i] = new ArrayList<>();
 		}
