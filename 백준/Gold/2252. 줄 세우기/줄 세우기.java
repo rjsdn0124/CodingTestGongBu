6e08e6ee6a2682e43f45ebc0fecc36c422a4723b
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-02-03 13:56:23 +0900

    [Gold III] Title: 줄 세우기, Time: 544 ms, Memory: 54056 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+public class Main{
+	private static int N;
+	private static StringBuilder sb = new StringBuilder();
+	private static List<Integer>[] arr;
+	private static int[] parentCount;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		init(br);
+		solution();
+		System.out.print(sb);
+	}
+
+	private static void solution(){
+		Queue<Integer> pq = new PriorityQueue<>();
+
+		for(int i = 1; i <= N; i++){
+			if(parentCount[i] == 0) {
+				pq.add(i);
+			}
+		}
+
+		while(!pq.isEmpty()){
+			int p = pq.poll();
+			sb.append(p).append(" ");
+
+			for(int c : arr[p]){
+				if(--parentCount[c] == 0){
+					pq.add(c);
+				}
+			}
+		}
+	}
+
+	private static void init(BufferedReader br) throws IOException {
+		String[] line = br.readLine().split(" ");
+		N = Integer.parseInt(line[0]);
+		int m = Integer.parseInt(line[1]);
+
+		arr = new ArrayList[N + 1];
+		parentCount = new int[N + 1];
+
+		for(int i = 1; i <= N; i++) {
+			arr[i] = new ArrayList<>();
+		}
+
+		for(int i = 0; i < m; i++) {
+			String[] l = br.readLine().split(" ");
+			int parent = Integer.parseInt(l[0]);
+			int child = Integer.parseInt(l[1]);
+			arr[parent].add(child);
+			parentCount[child]++;
+		}
+	}
+}
