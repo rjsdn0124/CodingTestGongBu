79fce3d2d6bd50b611f8b7e7a02f5750042635f3
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-02-04 16:06:10 +0900

    [Gold I] Title: 계단 수, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Gold/1562. 계단 수/계단 수.java
+++ 백준/Gold/1562. 계단 수/계단 수.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-	private static int N, maxBit = 1024, result = 0;
+	private static int N, hitInfo = 4, result = 0;
 	private static int[][][] dp;
 
 	public static void main(String[] args) throws IOException{
@@ -15,8 +15,10 @@ public class Main{
 
 	private static void solution(){
 		// [depth][0~9][bit]
-		for(int i = 0; i < 10; i++){
-			dp[0][i][1 << i] = 1;
+		dp[0][0][1] = 1;
+		dp[0][9][2] = 1;
+		for(int i = 1; i < 9; i++){
+			dp[0][i][0] = 1;
 		}
 
 		for(int i = 1; i < N; i++){
@@ -24,30 +26,30 @@ public class Main{
 		}
 
 		for(int i = 1; i < 10; i++){
-			result += dp[N - 1][i][maxBit - 1];
+			result += dp[N - 1][i][hitInfo - 1];
 			result %= 1000000000;
 		}
 	}
 
 	private static void updatedp(int depth){
-		addNextStep(depth, 0, 1);
+		addNextStep(depth, 0, 1, 1);
 		for(int i = 1; i < 9; i++){
-			addNextStep(depth, i, i + 1);
-			addNextStep(depth, i, i - 1);
+			addNextStep(depth, i, i + 1, 0);
+			addNextStep(depth, i, i - 1, 0);
 		}
-		addNextStep(depth, 9, 8);
+		addNextStep(depth, 9, 8, 2);
 	}
 
-	private static void addNextStep(int depth, int now, int next){
-		for(int i = 1; i < maxBit; i++){
-			dp[depth][now][i | 1 << now] += dp[depth - 1][next][i];
-			dp[depth][now][i | 1 << now] %= 1000000000;
+	private static void addNextStep(int depth, int now, int next, int hitPlace){
+		for(int i = 0; i < hitInfo; i++){
+			dp[depth][now][i | hitPlace] += dp[depth - 1][next][i];
+			dp[depth][now][i | hitPlace] %= 1000000000;
 		}
 	}
 
 	private static void init(BufferedReader br) throws IOException {
 		N = Integer.parseInt(br.readLine());
 
-		dp = new int[N][10][maxBit];
+		dp = new int[N][10][hitInfo];
 	}
 }
