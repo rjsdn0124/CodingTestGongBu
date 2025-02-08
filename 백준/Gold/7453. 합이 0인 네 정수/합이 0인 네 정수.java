324d5a412ce7bd275fda4990cb3a1d800fac2798
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-02-08 15:14:42 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4592 ms, Memory: 286140 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	private static int N;
+	private static long result = 0L;
+	private static int[][] arr;
+	private static long[][] sumArr;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		init(br);
+		solution();
+		System.out.println(result);
+	}
+
+	private static void solution(){
+		int ind = 0;
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j < N; j++){
+				sumArr[0][ind] = (long)arr[0][i] + arr[1][j];
+				sumArr[1][ind++] = (long)arr[2][i] + arr[3][j];
+			}
+		}
+
+		Arrays.sort(sumArr[0]);
+		Arrays.sort(sumArr[1]);
+
+		int lastInd = sumArr[1].length - 1;
+		long prev = Long.MIN_VALUE;
+		int sameCount = 0;
+		for(int i = 0; i < sumArr[0].length; i++){
+			if(prev == sumArr[0][i]){
+				result += sameCount;
+			}else {
+				prev = sumArr[0][i];
+				sameCount = 0;
+				while (lastInd >= 0) {
+					if (sumArr[1][lastInd] + sumArr[0][i] < 0) {
+						break;
+					} else if (sumArr[1][lastInd] + sumArr[0][i] == 0) {
+						sameCount++;
+						result++;
+					}
+					lastInd--;
+				}
+			}
+		}
+	}
+
+	private static void init(BufferedReader br) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		arr = new int[4][N];
+		sumArr = new long[2][N * N];
+
+		for(int i = 0; i < N; i++){
+			String[] line = br.readLine().split(" ");
+			for(int j = 0; j < 4; j++) {
+				arr[j][i] = Integer.parseInt(line[j]);
+			}
+		}
+	}
+}
