73fe4308b773e9f4647e01ec68dc3a6299b1f57a
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2025-02-10 22:18:29 +0900

    [Gold III] Title: 크게 만들기, Time: 384 ms, Memory: 34712 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.java
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.java
@@ -1,39 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        int len = Integer.parseInt(str[0]);
-        int del = Integer.parseInt(str[1]);
-        Stack<Character> stack = new Stack<>();
-        String input = br.readLine();
-        
-        for(int i = 0; i < len; i++){
-            if(stack.isEmpty()){
-                stack.push(input.charAt(i));
-            }else{
-                while(stack.peek() < input.charAt(i)){
-                    if(del == 0){
-                        break;
-                    }
-                    stack.pop();
-                    del--;
-                    if(stack.isEmpty()){
-                        break;
-                    }
-                }
-                stack.push(input.charAt(i));
-            }
-        }
-        char[] arr = new char[stack.size()];
-        int j = 0;
-        while(!stack.isEmpty()){
-            arr[j++] = stack.pop();
-        }
-        for(int i = arr.length - 1; i >= del; i--){
-            System.out.print(arr[i]);
-        }
-    }
+public class Main{
+	private static int N, K;
+	private static StringBuilder sb = new StringBuilder();
+	private static char[] arr;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		init(br);
+		solution();
+		System.out.println(sb);
+	}
+
+	private static void solution(){
+		// 인덱스 올라가면서 다음에 작아질 때 하나씩 빼주기
+		Stack<Character> st = new Stack<>();
+		for(int i = 0; i < N; i++){
+			while(K > 0 && !st.isEmpty() && st.peek() < arr[i]){
+				st.pop();
+				K--;
+			}
+			st.push(arr[i]);
+		}
+
+		while(!st.isEmpty()){
+			if(K-- > 0){
+				st.pop();
+			}else {
+				sb.append(st.pop());
+			}
+		}
+		sb.reverse();
+	}
+
+	private static void init(BufferedReader br) throws IOException {
+		String[] line = br.readLine().split(" ");
+		N = Integer.parseInt(line[0]);
+		K = Integer.parseInt(line[1]);
+
+		arr = br.readLine().toCharArray();
+	}
 }
